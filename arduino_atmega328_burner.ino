480b7b66d813f582061da5f96b1650aef55cff85
Austin Thresher <austin@dangermomentum.com>
2016-11-09 06:55:10 -0700

    Initial commit

---
+++ arduino_atmega328_burner.ino
@@ -0,0 +1,459 @@
+// Firmware for an atmega328 based EEPROM programmer.
+// Written and tested with an Atmel 28c256, but should
+// work with other pin-compatible chips.
+
+
+// ----------------
+// Macros and types
+// ----------------
+
+#define FORCE_INLINE __attribute__((always_inline))
+
+#define PIN_A0 14
+#define PIN_A1 15
+#define PIN_A2 16
+#define PIN_A3 17
+#define PIN_A4 18
+#define PIN_A5 19
+
+// Shift registers
+#define SERIAL_PIN PIN_A0
+#define LATCH_PIN  PIN_A2
+#define SHIFT_PIN  PIN_A3
+
+// EEPROM
+#define WRITE_PIN  PIN_A4
+#define READ_PIN   PIN_A5
+#define CHIP_PIN   PIN_A1
+
+// Status LEDs
+#define WRITE_LED 10
+#define READ_LED  11
+
+#define ushort uint16_t
+
+#define BUFFER_SIZE 32
+
+enum State {
+   REST,
+   READ,
+   WRITE,
+   ERR
+};
+
+
+// --------------------
+// Variable definitions
+// --------------------
+
+enum State state = REST;
+int blink_timer  = 0;
+int bytes_written   = 0;
+ushort addr      = 0x0000;
+byte data        = 0xFF;
+bool wait        = true;
+byte page_buffer[64];
+byte ascii_buffer[128];
+
+// -------------------
+// Function prototypes
+// -------------------
+
+inline void shiftByte(byte data) FORCE_INLINE;
+inline void setAddrBus(ushort addr) FORCE_INLINE;
+inline void setDataBus(byte val) FORCE_INLINE;
+inline void setWriteMode() FORCE_INLINE;
+inline void setReadMode() FORCE_INLINE;
+inline void writeToAddr(ushort address, byte value) FORCE_INLINE;
+inline byte readFromAddr(ushort address) FORCE_INLINE;
+inline byte readDataBus() FORCE_INLINE;
+inline char nibbleToASCII(byte val) FORCE_INLINE;
+inline byte ASCIIToNibble(char digit) FORCE_INLINE;
+void fillASCIIBuffer(byte *source, byte *dest, int bin_count);
+void fillBinBuffer(byte *source, byte *dest, int ascii_count);
+
+
+// ----------------
+// ASCII conversion
+// ----------------
+
+char nibbleToASCII(byte val) {
+   switch(val) {
+      case 0x0: return '0';
+      case 0x1: return '1';
+      case 0x2: return '2';
+      case 0x3: return '3';
+      case 0x4: return '4';
+      case 0x5: return '5';
+      case 0x6: return '6';
+      case 0x7: return '7';
+      case 0x8: return '8';
+      case 0x9: return '9';
+      case 0xA: return 'A';
+      case 0xB: return 'B';
+      case 0xC: return 'C';
+      case 0xD: return 'D';
+      case 0xE: return 'E';
+      case 0xF: return 'F';
+   }
+   return '0';
+}
+
+byte ASCIIToNibble(char digit) {
+   switch(digit) {
+      case '0': return 0;
+      case '1': return 1;
+      case '2': return 2;
+      case '3': return 3;
+      case '4': return 4;
+      case '5': return 5;
+      case '6': return 6;
+      case '7': return 7;
+      case '8': return 8;
+      case '9': return 9;
+      case 'A': return 10;
+      case 'B': return 11;
+      case 'C': return 12;
+      case 'D': return 13;
+      case 'E': return 14;
+      case 'F': return 15;
+   }
+   return 0;
+}
+
+// Convert a binary buffer to ASCII
+void fillASCIIBuffer(byte *source, byte *dest, int bin_count) {
+   for (int i = 0; i < bin_count; ++i) {
+      dest[i * 2]     = nibbleToASCII(source[i] >> 4); // hi
+      dest[i * 2 + 1] = nibbleToASCII(source[i] & 0xF); // lo
+   }
+}
+
+// Convert an ASCII buffer to binary
+void fillBinBuffer(byte *source, byte *dest, int ascii_count) {
+   for (int i = 0; i < ascii_count; ++i) {
+      if (i % 2 == 0) { // hi
+         dest[i / 2] = ASCIIToNibble(source[i]) << 4;
+      } else { // lo
+         dest[i / 2] += ASCIIToNibble(source[i]);
+      }
+   }
+}
+
+
+// --------------------
+// Bus I/O 
+// --------------------
+
+
+// These Bus I/O methods are modified versions of the ones
+// found in MEEPROMMERfirmware.ino from the MEEPROMMER source:
+//    https://github.com/mkeller0815/MEEPROMMER
+
+
+// Writes a single byte to the shift registers
+void shiftByte(byte val) {
+   for (int i = 0; i < 8; ++i) {
+      digitalWrite(SHIFT_PIN, LOW);
+      digitalWrite(SERIAL_PIN, val & (0x80 >> i));
+      digitalWrite(SHIFT_PIN, HIGH);
+   }
+}
+
+
+// Places a 16 bit address on the shift registers.
+// Writing hi or lo first may need to be swapped
+// depending on wiring. The 28c256 only uses the
+// lower 15 bits.
+void setAddrBus(ushort addr) {
+   byte hi = addr >> 8;
+   byte lo = addr & 0xFF;
+   digitalWrite(LATCH_PIN, LOW);
+   shiftByte(lo);
+   shiftByte(hi);
+   digitalWrite(LATCH_PIN, HIGH);
+}
+
+
+// Places the byte on the data bus.
+void setDataBus(byte val) {
+   digitalWrite(8, (val >> 6) & 0x01);
+   digitalWrite(9, (val >> 7) & 0x01);
+   // MEEPROMMER doesn't mask PIND, this fixes some bad writes
+   PORTD = (PIND & 3) | (val << 2); 
+}
+
+byte readDataBus() {
+   return digitalRead(2)
+        |(digitalRead(3) << 1)
+        |(digitalRead(4) << 2)
+        |(digitalRead(5) << 3)
+        |(digitalRead(6) << 4)
+        |(digitalRead(7) << 5)
+        |(digitalRead(8) << 6)
+        |(digitalRead(9) << 7);
+}
+
+
+// Set the data bus pins to output data
+void setWriteMode() {
+   for (int i = 0; i < 8; ++i) {
+      pinMode(2 + i, OUTPUT);
+   }
+}
+
+
+// Set the data bus pins to read data
+void setReadMode() {
+   for (int i = 0; i < 8; ++i) {
+      pinMode(2 + i, INPUT);
+   }
+}
+
+
+// -------------
+// EEPROM Access
+// -------------
+
+void writePage(ushort address, byte *buffer, int num) {
+   digitalWrite(READ_PIN, LOW);
+   digitalWrite(CHIP_PIN, HIGH);
+   digitalWrite(WRITE_PIN, HIGH);
+   digitalWrite(READ_PIN, HIGH);
+
+   for (int i = 0; i < num; ++i) {
+      setAddrBus(address + i);
+      setDataBus(buffer[i]);
+      delayMicroseconds(1);
+      digitalWrite(CHIP_PIN, LOW);
+      digitalWrite(WRITE_PIN, LOW);  
+      delayMicroseconds(1);
+      digitalWrite(WRITE_PIN, HIGH);
+      digitalWrite(CHIP_PIN, HIGH);
+   }
+}
+
+// Writes a byte using BUFFER_SIZE write timing.
+// Data pins must be in write mode before calling this.
+void writeToAddr(ushort address, byte value) {
+   digitalWrite(READ_PIN, LOW);
+   digitalWrite(CHIP_PIN, HIGH);
+   digitalWrite(WRITE_PIN, HIGH);
+   digitalWrite(READ_PIN, HIGH);
+   setAddrBus(address);
+   setDataBus(value);
+   delayMicroseconds(1);
+   digitalWrite(CHIP_PIN, LOW);
+   digitalWrite(WRITE_PIN, LOW);  
+   delayMicroseconds(1);
+   digitalWrite(WRITE_PIN, HIGH);
+   digitalWrite(CHIP_PIN, HIGH);
+}
+
+
+// Reads a byte from the given address.
+// Data pins must be in read mode before calling this.
+byte readFromAddr(ushort address) {
+   setAddrBus(address);
+   digitalWrite(CHIP_PIN, HIGH);
+   digitalWrite(WRITE_PIN, HIGH);
+   digitalWrite(READ_PIN, HIGH);
+   digitalWrite(CHIP_PIN, LOW);
+   digitalWrite(READ_PIN, LOW);
+   delayMicroseconds(1);
+   byte value = readDataBus();
+   digitalWrite(READ_PIN, HIGH);
+   digitalWrite(CHIP_PIN, HIGH);
+   return value;
+}
+
+
+// This disables the write protect mode on the Atmel 28c256.
+// Write mode must be enabled before calling this.
+void disableProtectMode() {
+   digitalWrite(READ_PIN, HIGH);
+   writeToAddr(0x5555, 0xAA);
+   writeToAddr(0x2AAA, 0x55);
+   writeToAddr(0x5555, 0x80);
+   writeToAddr(0x5555, 0xAA);
+   writeToAddr(0x2AAA, 0x55);
+   writeToAddr(0x5555, 0x20);
+}
+
+
+// --------------------------
+// Arduino built-in functions
+// --------------------------
+
+
+void setup() {
+   pinMode(SERIAL_PIN, OUTPUT);
+   pinMode(LATCH_PIN,  OUTPUT);
+   pinMode(SHIFT_PIN,  OUTPUT);
+   pinMode(WRITE_PIN,  OUTPUT);
+   pinMode(READ_PIN,   OUTPUT);
+   pinMode(CHIP_PIN,   OUTPUT);
+   pinMode(WRITE_LED,  OUTPUT);
+   pinMode(READ_LED,   OUTPUT);
+   digitalWrite(READ_PIN,   HIGH);
+   digitalWrite(CHIP_PIN,   HIGH);
+   digitalWrite(WRITE_PIN,  HIGH);
+
+   Serial.begin(9600);
+}
+
+
+void loop() {
+
+   // Wait for a command over serial
+   if (wait) {
+
+      // These pins are all active when low
+      digitalWrite(CHIP_PIN,   HIGH);
+      digitalWrite(WRITE_PIN,  HIGH);
+      digitalWrite(READ_PIN,   HIGH);
+      setAddrBus(0);
+      setDataBus(0);
+
+      switch (state) {
+         case ERR:
+            for (int i = 0; i < 3; ++i) {
+               digitalWrite(READ_LED, HIGH);
+               digitalWrite(WRITE_LED, HIGH);
+               delay(250);
+               digitalWrite(READ_LED, LOW);
+               digitalWrite(WRITE_LED, LOW);
+               delay(250);
+            }
+            state = REST;
+            break;
+            
+         default:
+            digitalWrite(READ_LED, HIGH);
+            digitalWrite(WRITE_LED, HIGH);
+            delay(10);
+            break;
+      }
+   }
+   else {
+      switch(state) {
+         case WRITE:
+            while (state == WRITE) {
+               digitalWrite(CHIP_PIN,   HIGH);
+               digitalWrite(WRITE_PIN,  HIGH);
+               digitalWrite(READ_PIN,   LOW);
+               int timeout = 0;
+               int blinker = 0;
+               int curaddr = addr;
+               // Send the current address to ensure we're synced
+               Serial.write((byte*)&addr, 2);
+               while (!Serial.available()) {
+                  delay(1);
+                  blinker++;
+                  if (blinker > 200) {
+                     blinker = 0;
+                     if (addr != 0) { // Only timeout after receiving data
+                        timeout++;
+                        if (timeout > 15) { // 3 second timeout
+                           state = ERR;
+                           wait = true;
+                           return;
+                        }
+                     }
+                  }
+                  digitalWrite(WRITE_LED, blinker < 100 ? HIGH : LOW);
+               }
+               
+               digitalWrite(WRITE_LED, LOW);
+               int ascii_bytes =
+                  Serial.readBytes((char*)ascii_buffer, BUFFER_SIZE * 2); 
+               fillBinBuffer(ascii_buffer, page_buffer, ascii_bytes);
+               int bytes_to_write = ascii_bytes / 2;
+               bool validated = false;
+               while (!validated) {
+                  validated = true;
+                  digitalWrite(WRITE_LED, HIGH);
+                  setWriteMode();
+                  writePage(curaddr, page_buffer, bytes_to_write);
+                  delay(20);
+                  setReadMode();
+                  for (int i = 0; i < bytes_written; ++i) {
+                     byte val = readFromAddr(curaddr+i);
+                     if (val != page_buffer[i]) {
+                        if (validated) {
+                           addr = curaddr;
+                           validated = false;
+                        }
+                        digitalWrite(WRITE_LED, LOW);
+                        delay(50);
+                        digitalWrite(WRITE_LED, HIGH);
+                        break;
+                     }
+                  }
+               }
+               addr += bytes_to_write;
+               if (addr >= 0x8000) {
+                 state = REST;
+                 wait = true;
+               }
+           }
+           break;
+        case READ:
+           while (state == READ) {
+             for (int i = 0; i < BUFFER_SIZE; i++) {
+                page_buffer[i] = readFromAddr(addr++);
+             }
+             fillASCIIBuffer(page_buffer, ascii_buffer, BUFFER_SIZE);
+             Serial.write(ascii_buffer, BUFFER_SIZE * 2);
+             if (addr >= 0x8000) {
+                state = REST;
+                wait = true;
+             }
+           }
+           break;
+        default:
+           wait = true;
+      }
+   }
+}
+
+
+void serialEvent() {
+  while (Serial.available() && state == REST) {
+      char command = Serial.read();
+      switch (command) {
+      case 'R': // Read EEPROM contents
+         state = READ;
+         addr  = 0x0000;
+         data  = 0x00;
+         wait  = false;
+         setReadMode();
+         digitalWrite(WRITE_LED, LOW);
+         digitalWrite(READ_LED, HIGH);
+         delay(1500);
+         return;
+      case 'E': // Erase EEPROM
+         // TODO
+         break;
+      case 'U': // Unlock write protected EEPROM 
+         setWriteMode();
+         digitalWrite(WRITE_LED, HIGH);
+         digitalWrite(READ_LED, LOW);
+         disableProtectMode();
+         state = REST;
+         wait = true;
+         return;
+      case 'W': // Write data to EEPROM 
+         state = WRITE;
+         addr  = 0x0000;
+         data  = 0x00;
+         wait  = false;
+         digitalWrite(WRITE_LED, HIGH);
+         digitalWrite(READ_LED, LOW);
+         setWriteMode();
+         bytes_written = BUFFER_SIZE;
+         return;
+      }
+      state = ERR; 
+   }
+}
---
+++ eeprom_burner.c
@@ -0,0 +1,254 @@
+#include <stdint.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+
+#define CHIP_SIZE 0x8000
+#define BUFFER_SIZE 32
+#define FILENAME "file.bin"
+
+enum Command {
+   READ,
+   WRITE,
+   ERASE,
+   UNLOCK
+};
+
+enum Command cmd;
+
+// TODO: Write functions for sending and receiving ascii buffers as binary
+
+void exit_usage(char* arg0) {
+   printf("USAGE: %s [-W | -R | -E | -U] [serial port] [filename]\n", arg0);
+   exit(1);
+}
+
+uint8_t nibble_to_ascii(uint8_t val) {
+   switch(val) {
+      case 0x0: return '0';
+      case 0x1: return '1';
+      case 0x2: return '2';
+      case 0x3: return '3';
+      case 0x4: return '4';
+      case 0x5: return '5';
+      case 0x6: return '6';
+      case 0x7: return '7';
+      case 0x8: return '8';
+      case 0x9: return '9';
+      case 0xA: return 'A';
+      case 0xB: return 'B';
+      case 0xC: return 'C';
+      case 0xD: return 'D';
+      case 0xE: return 'E';
+      case 0xF: return 'F';
+   }
+   return '0';
+}
+
+uint8_t ascii_to_nibble(char digit) {
+   switch(digit) {
+      case '0': return 0;
+      case '1': return 1;
+      case '2': return 2;
+      case '3': return 3;
+      case '4': return 4;
+      case '5': return 5;
+      case '6': return 6;
+      case '7': return 7;
+      case '8': return 8;
+      case '9': return 9;
+      case 'A': return 10;
+      case 'B': return 11;
+      case 'C': return 12;
+      case 'D': return 13;
+      case 'E': return 14;
+      case 'F': return 15;
+   }
+   return 0;
+}
+
+// Convert a binary buffer to ASCII
+void fill_ascii_buffer(uint8_t *source, char *dest, int bin_count) {
+   for (int i = 0; i < bin_count; ++i) {
+      dest[i * 2]     = nibble_to_ascii(source[i] >> 4); // hi
+      dest[i * 2 + 1] = nibble_to_ascii(source[i] & 0xF); // lo
+   }
+}
+
+// Convert an ASCII buffer to binary
+void fill_bin_buffer(char *source, uint8_t *dest, int ascii_count) {
+   for (int i = 0; i < ascii_count; ++i) {
+      if (i % 2 == 0) { // hi
+         dest[i / 2] = ascii_to_nibble(source[i]) << 4;
+      } else { // lo
+         dest[i / 2] += ascii_to_nibble(source[i]);
+      }
+   }
+}
+
+int send(int file, void *data, int count) {
+   int sent = write(file, data, count);
+   if (sent != count) {
+      perror("send");
+      fprintf(stderr, "Error: Tried to send %d, but only sent %d.\n",
+            count, sent);
+      close(file);
+      exit(1);
+   }
+   return sent;
+}
+
+int main(int argc, char** argv) {
+
+   // Parse and verify arguments
+   if (argc < 3) {
+      exit_usage(argv[0]);
+   }
+
+   if (argv[1][0] != '-') {
+      exit_usage(argv[0]);
+   }
+
+   switch (argv[1][1]) {
+      case 'R':
+         cmd = READ;
+         break;
+      case 'W':
+         cmd = WRITE;
+         break;
+      case 'E':
+         cmd = ERASE;
+         break;
+      case 'U':
+         cmd = UNLOCK;
+         break;
+      default:
+         exit_usage(argv[0]);
+   }
+
+   char *filename = NULL;
+   if (argc == 4) {
+      filename = argv[3];
+   }
+   else {
+      printf("Using default filename %s\n", FILENAME);
+   }
+
+   // Open serial communication
+   int com = open(argv[2], O_RDWR | O_NOCTTY /*| O_NDELAY*/);
+   if (com < 0) {
+      perror(argv[2]);
+      exit(1);
+   }
+   fcntl(com, F_SETFL, 0); // We want reads to be blocking 
+
+   struct termios options;
+   tcgetattr(com, &options);
+   cfsetispeed(&options, B9600);
+   cfsetospeed(&options, B9600);
+   options.c_cflag |= CLOCAL | CREAD;
+   options.c_cflag &= ~PARENB; // No parity
+   options.c_cflag &= ~CSTOPB; // One stop bit
+   options.c_cflag &= ~CSIZE; // 8 bit characters
+   options.c_cflag |= CS8;
+   options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // Raw input
+   options.c_iflag &= ~(IXON | IXOFF | IXANY); // No flow control
+   options.c_oflag &= ~OPOST; // Raw output
+   options.c_cc[VMIN] = BUFFER_SIZE * 2;
+   options.c_cc[VTIME] = 50;
+
+   tcsetattr(com, TCSANOW, &options);
+   printf("Opened %s\n", argv[2]);
+   sleep(2);
+
+   uint8_t buf[BUFFER_SIZE];
+   char ascii_buf[BUFFER_SIZE * 2];
+   int addr  = 0;
+   int recvd = 0;
+   int file;
+   switch (cmd) {
+      case READ:
+         file = open(filename == NULL ? FILENAME : filename,
+               O_RDWR | O_CREAT);
+         if (file < 0) {
+            perror("read");
+            fprintf(stderr, "Error: couldn't create output file.\n");
+            close(com);
+            exit(1);
+         }
+         printf("Sending READ command\n");
+         send(com, "R", 1);
+         sleep(1);
+         printf("Receiving data\n");
+         while (addr < CHIP_SIZE) {
+            printf("%04X ", addr);
+            recvd = read(com, ascii_buf, BUFFER_SIZE*2);
+            if (recvd % 2 == 1) {
+               fprintf(stderr, "\nError: Odd number of bytes\n");
+               close(file);
+               close(com);
+               exit(1);
+            }
+            fill_bin_buffer(ascii_buf, buf, recvd);
+            write(file, buf, recvd / 2);
+            addr += recvd / 2;
+            if (addr % 0x200 == 0) {
+               printf("\n");
+            }
+            fflush(stdout);
+         }
+         close(file);
+         printf("\nWrote %d bytes to %s\n", addr,
+               filename == NULL ? FILENAME : filename);
+         break;
+
+      case WRITE:
+         if (filename == NULL) {
+            fprintf(stderr, "Error: No input file.\n");
+            close(com);
+            exit_usage(argv[0]);
+         }
+         file = open(filename, O_RDONLY);
+         if (file < 0) {
+            perror("write");
+            fprintf(stderr, "Error: couldn't open input file.\n");
+            close(com);
+            exit(1);
+         }
+         printf("Sending WRITE command\n");
+         send(com, "W", 1);
+         sleep(1);
+         printf("Sending data\n");
+         while (addr < CHIP_SIZE) {
+            uint16_t recvd_addr;
+            read(com, &recvd_addr, 2);
+            addr = recvd_addr;
+            if (addr != recvd_addr) {
+               fprintf(stderr, "Error: Address mismatch (%04X != %04X)\n",
+                     recvd_addr, addr);
+               close(file);
+               close(com);
+               exit(1);
+            }
+            printf("%04X ", addr);
+            lseek(file, addr, SEEK_SET);
+            read(file, buf, BUFFER_SIZE); // TODO: Check for EOF
+            fill_ascii_buffer(buf, ascii_buf, BUFFER_SIZE);
+            send(com, ascii_buf, BUFFER_SIZE * 2);
+            addr += BUFFER_SIZE;
+            if (addr % 0x200 == 0) {
+               printf("\n");
+            }
+            fflush(stdout);
+         }
+         close(file);
+         printf("\nSent %d bytes to %s\n", addr, argv[2]);
+         break;
+   }
+   close(com);
+   return 0;
+}
